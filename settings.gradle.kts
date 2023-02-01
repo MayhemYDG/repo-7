e779da39b57f8411e29dfb5a61cdcff41d7954b4
rarawat <rarawat@twilio.com>
2023-02-01 17:02:55 +0530

    LIBMOBILE-1188
    - Initial Readme changes
    - updated README.md, settings.gradle.kts, gradle.properties, source package
    - deleted MyDestination, MyDestinationTests
    - created NielsenDTVRDestination, NielsenDTVRDestinationTests

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "analytics-kotlin-destination-template"
+rootProject.name = "analytics-kotlin-integration-nielsen-dtvr"
 include(":lib")
 includeBuild("publishing-plugins")
 include(":testapp")
